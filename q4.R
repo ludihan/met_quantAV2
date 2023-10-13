7c73946ef27e0fbc77aa1b74496291e1e5df6c94
ludihan <luccadhan@gmail.com>
2023-10-13 17:16:52 -0300

    questao 4 feita

---
+++ q4.R
@@ -0,0 +1,16 @@
+#Passo 1:
+
+equacao_regressao = function(X) {
+  a = (20 * 1083.67 - 1478 * 12.75) / 
+    (20 * 143215.8 - (1478 ** 2))
+  b = (12.75 / 20) - a * (1478 / 20)
+  # (0.004161175 * X + 0.3299892)
+  return(a * X + b)
+}
+
+#Passo 2:
+equacao_regressao(85)
+
+#Passo 3:
+equacao_regressao(85 + 1)
+# Deformaria mais
