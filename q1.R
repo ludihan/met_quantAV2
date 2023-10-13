ff43c364c96945ada9bd8d09607559a12311e824
ludihan <luccadhan@gmail.com>
2023-10-13 08:30:41 -0300

    questao 1

--- q1.R
+++ q1.R
@@ -1,4 +1,28 @@
+library(ggplot2)
+
 trafego = c(3,3,5,5,10,10,15,15,20,20,25,25,30,30)
 velocidade = c(95.6, 93.8, 74.4, 74.4, 50.5, 51.5, 44.6, 42.6, 35.8, 38.7, 32.0, 32.0, 30.1, 29.1)
-abline(lm(velocidade ~ trafego))
 plot(trafego, velocidade)
+abline(lm(velocidade ~ trafego))
+
+dados = data.frame(
+  trafego = c(3,3,5,5,10,10,15,15,20,20,25,25,30,30),
+  velocidade = c(95.6, 93.8, 74.4, 74.4, 50.5, 51.5, 44.6, 42.6, 35.8, 38.7, 32.0, 32.0, 30.1, 29.1)
+)
+
+#Passo 1 e 2:
+
+grafico_dispersao = ggplot(dados, aes(x = trafego, y = velocidade)) + 
+  geom_smooth(method='lm', se=FALSE) +
+  geom_point() + #pontos de dispersão
+  labs(x = "Volume de Tráfego", y = "Velocidade Média") +
+  ggtitle("Diagrama de Dispersão de Volume de Tráfego vs. Velocidade Média")
+
+print(grafico_dispersao)
+
+#Passo 3:
+
+cor(dados$trafego,dados$velocidade)
+
+#Passo 4: 
+#TODO
